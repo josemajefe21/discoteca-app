742630c0e9dbcf134ab2f795efa3fc0162505ec4
josemajefe21 <49629037+josemajefe21@users.noreply.github.com>
2025-09-22 15:50:17 -0300

    feat(build): plugin legacy para compatibilidad y HTML con bundles fallback

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      renderLegacyChunks: true,
+      modernPolyfills: true,
+    }),
+  ],
 })
